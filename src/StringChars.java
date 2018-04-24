732d4179ad7ab8191d277b6bfa3380b0036fa301
SimonPreisiner <simonpreisinger@live.de>
2018-04-24 07:44:33 +0200

    nested class

--- src/StringChars.java
+++ src/StringChars.java
@@ -9,6 +9,49 @@ public class StringChars implements Iterable<Character>{
 
     @Override
     public Iterator<Character> iterator() {
-        return new StringCharsIterator(theString);
+        return new HalloSusi(theString);
+    }
+
+    private static class FastoSusi implements Iterator<Character> {
+        private final String theString;
+
+        private int position;
+
+        public FastoSusi(String theString) {
+            this.theString = theString;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return position < theString.length();
+        }
+
+        @Override
+        public Character next() {
+            position++;
+            position++;
+            return theString.charAt(position - 1);
+        }
+    }
+
+    private static class HalloSusi implements Iterator<Character> {
+        private final String theString;
+
+        private int position;
+
+        public HalloSusi(String theString) {
+            this.theString = theString;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return position < theString.length();
+        }
+
+        @Override
+        public Character next() {
+            position++;
+            return theString.charAt(position - 1);
+        }
     }
 }
